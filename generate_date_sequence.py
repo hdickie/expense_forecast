f7ccd4ec4d3accbb4ad523d75d3c6009681e1005
Hume Dickie <hume.dickie@live.com>
2024-01-07 04:12:30 -0800

    fixed semiweekly, quarterly and yearly cadences

--- generate_date_sequence.py
+++ generate_date_sequence.py
@@ -4,12 +4,7 @@ import datetime
 def generate_date_sequence(start_date_YYYYMMDD, num_days, cadence):
     """ A wrapper for pd.date_range intended to make code easier to read.
 
-    #todo write project_utilities.generate_date_sequence() doctests
     """
-    #log_in_color('green', 'debug', 'ENTER generate_date_sequence(start_date_YYYYMMDD=' + str(start_date_YYYYMMDD) + ',num_days=' + str(num_days) + ',=' + str(cadence) + '):', 0)
-    # print('start_date_YYYYMMDD:'+str(start_date_YYYYMMDD))
-    # print('num_days...........:'+str(num_days))
-    # print('cadence............:'+str(cadence))
 
     start_date = datetime.datetime.strptime(start_date_YYYYMMDD, '%Y%m%d')
     end_date = start_date + datetime.timedelta(days=num_days)
@@ -21,24 +16,31 @@ def generate_date_sequence(start_date_YYYYMMDD, num_days, cadence):
     elif cadence.lower() == "weekly":
         return_series = pd.date_range(start_date, end_date, freq='W')
     elif cadence.lower() == "semiweekly":
-        return_series = pd.date_range(start_date, end_date, freq='2W')
+        #return_series = pd.date_range(start_date, end_date, freq='2W')
+        day_delta = int(start_date.strftime('%d')) - 2 #not sure why this is 2 when others needed 1 but testing says so
+        start_date = start_date - datetime.timedelta(days=day_delta)
+        relevant_semiweekly_schedule = pd.date_range(start_date, end_date, freq='2W')
+        return_series = relevant_semiweekly_schedule + datetime.timedelta(days=day_delta)
     elif cadence.lower() == "monthly":
-
         day_delta = int(start_date.strftime('%d')) - 1
-        first_of_each_relevant_month = pd.date_range(start_date - datetime.timedelta(days=day_delta), end_date, freq='MS')
-
+        start_date = start_date - datetime.timedelta(days=day_delta)
+        first_of_each_relevant_month = pd.date_range(start_date, end_date, freq='MS')
         return_series = first_of_each_relevant_month + datetime.timedelta(days=day_delta)
     elif cadence.lower() == "quarterly":
-        # todo check if this needs an adjustment like the monthly case did
-        return_series = pd.date_range(start_date, end_date, freq='Q')
+        day_delta = int(start_date.strftime('%d'))
+        start_date = start_date - datetime.timedelta(days=day_delta)
+        first_of_each_relevant_quarter = pd.date_range(start_date, end_date,freq='Q')
+        return_series = first_of_each_relevant_quarter + datetime.timedelta(days=day_delta)
     elif cadence.lower() == "yearly":
-        # todo check if this needs an adjustment like the monthly case did
-        return_series = pd.date_range(start_date, end_date, freq='Y')
+        day_delta = int(start_date.strftime('%d')) - 1
+        start_date = start_date - datetime.timedelta(days=day_delta)
+
+        # Y is year end, YS is year start. there is no just 'year' option
+        first_of_each_relevant_year = pd.date_range(start_date, end_date, freq='YS')
+
+        return_series = first_of_each_relevant_year + datetime.timedelta(days=day_delta)
     else:
         raise ValueError("Undefined cadence in generate_date_sequence: "+str(cadence))
 
     return_series = [ d.strftime('%Y%m%d') for d in return_series ]
-
-    # log_in_color('green', 'debug', str(return_series), 0)
-    # log_in_color('green', 'debug', 'EXIT generate_date_sequence()', 0)
     return return_series
