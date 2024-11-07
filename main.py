eebaf69176b8443e25597bdf07ef85858dc4ac90
Hume Dickie <hume.dickie@live.com>
2024-11-06 22:55:54 -0800

    sync?

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-
+import sys
 if __name__ == '__main__':
     pass
     # From my understanding of the traditional role of a main.py, this project could use one.
     # Therefore, todo.
+    print('Python %s on %s' % (sys.version, sys.platform))
 
