b82fe8a7342cd433feda66e81f55972884d605d0
Hume Dickie <hume.dickie@live.com>
2024-11-07 18:22:08 -0800

    provoke merge

--- tests/_e2e/test_E2E.py
+++ tests/_e2e/test_E2E.py
@@ -4,9 +4,11 @@ class TestE2E:
     pass
 
     @pytest.mark.E2E
-    @pytest.mark.skip(reason="not yet implemented")
+    #@pytest.mark.skip(reason="not yet implemented")
     def test_E2E_pay_off_credit_card(self):
-        pass
+        raise NotImplementedError
+
+# ;count_skipped_tests = len();print(count_skipped_tests)
 
     @pytest.mark.E2E
     @pytest.mark.skip(reason="not yet implemented")
@@ -52,3 +54,5 @@ class TestE2E:
     @pytest.mark.skip(reason="not yet implemented")
     def test_E2E_privacy(self):
         pass
+
+    "from junitparser import JUnitXml;xml = JUnitXml.fromfile('test_results.xml');skipped_tests = len([case for suite in xml for case in suite if case.result and case.result[0].type == 'pytest.skip']);print(str(len(xml.tests)));print(str(len(skipped_tests)))"
